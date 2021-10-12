b1ee5b2b78bcc0e49078784b5eacbba168785ada
Ivan-Sotnikov <91842322+Ivan-Sotnikov@users.noreply.github.com>
2021-10-12 20:41:04 +0300

    New modal, login page
    Added a ndw modal window and new login page. Updated JS file.

--- all.js
+++ all.js
@@ -1,21 +1,30 @@
-function password_visibility(){
-    var input= document.getElementById("pass");
-    if (input.getAttribute("type") == "password"){
-        input.setAttribute("type", "text");
-    }
-    else {
-        input.setAttribute( "type", "password");
-    }
-    
-} 
-function show_hide_password(target){
-	var input = document.getElementById('pass');
-	if (input.getAttribute('type') == 'password') {
-		target.classList.add('view');
-		input.setAttribute('type', 'text');
-	} else {
-		target.classList.remove('view');
-		input.setAttribute('type', 'password');
-	}
-	return false;
+console.log("JS file is loaded");
+function password_visibility() {
+  var input = document.getElementById("pass");
+
+  if (input.getAttribute("type") == "password") {
+    input.setAttribute("type", "text");
+    document.getElementById("visib-btn").innerHTML = "visibility_off";
+  } else {
+    input.setAttribute("type", "password");
+    document.getElementById("visib-btn").innerHTML = "visibility";
+  }
 }
+function spaceship_souz() {
+  let souz = document.getElementById("spaceship");
+  souz.addEventListener("click", show_modal);
+  function show_modal() {
+    let visibility = document.getElementById("modal_spaceship");
+    let close = document.getElementById("modal_spaceship_close");
+	visibility.setAttribute("class",
+    	"modal-wrap modal-wrap_visible modal-wrap_style");
+    close.addEventListener("click", close_modal);
+	function close_modal() {
+      visibility.setAttribute("class",
+      "modal-wrap modal-wrap_hidden modal-wrap_style");
+	  event.stopPropagation();
+
+    }
+  }
+}
+spaceship_souz();
